993902ef7a2f521e5f22b1ec6e1e4bcae31ac3b4
Maxim <triznamaxim@gmail.com>
2024-01-21 13:56:21 +0700

    init

--- DataFrame_OneHot.py
+++ DataFrame_OneHot.py
@@ -1,3 +1,6 @@
+# В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца.
+# Ваша задача перевести его в one hot вид. Сделать без встроенных ф-ций, например,get_dummies.
+
 import pandas as pd
 import random
 
@@ -13,5 +16,3 @@ data.loc[data['whoAmI'] != 'robot', 'robot'] = '0'
 data.loc[data['whoAmI'] == 'human', 'human'] = '1'
 data.loc[data['whoAmI'] != 'human', 'human'] = '0'
 print(data.drop(['whoAmI'], axis=1))
-
-
