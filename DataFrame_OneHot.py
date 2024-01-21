21a12d8d0ac31eb6ada76cb246be8abe76ba8623
Maxim <triznamaxim@gmail.com>
2024-01-21 13:36:04 +0700

    init

---
+++ DataFrame_OneHot.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print(data)
+print()
+
+data.loc[data['whoAmI'] == 'robot', 'robot'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robot'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human'] = '1'
+data.loc[data['whoAmI'] != 'human', 'human'] = '0'
+print(data.drop(['whoAmI'], axis=1))
+
+
